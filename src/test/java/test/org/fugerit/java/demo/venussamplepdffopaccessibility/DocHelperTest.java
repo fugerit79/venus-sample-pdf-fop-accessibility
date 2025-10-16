6f091489dc02601d83971621d6efe555ff6ed7af
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2025-10-16 16:56:30 +0200

    feat: added PDF/UA with watermark

--- src/test/java/test/org/fugerit/java/demo/venussamplepdffopaccessibility/DocHelperTest.java
+++ src/test/java/test/org/fugerit/java/demo/venussamplepdffopaccessibility/DocHelperTest.java
@@ -1,6 +1,8 @@
 // generated from template 'DocHelperTest.ftl' on 2025-10-03T10:02:02.619+02:00
 package test.org.fugerit.java.demo.venussamplepdffopaccessibility;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.font.Standard14Fonts;
 import org.fugerit.java.core.io.FileIO;
 import org.fugerit.java.demo.venussamplepdffopaccessibility.DocHelper;
 import org.fugerit.java.demo.venussamplepdffopaccessibility.People;
@@ -15,13 +17,18 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import lombok.Getter;
-import lombok.AllArgsConstructor;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.graphics.state.PDExtendedGraphicsState;
+import org.apache.pdfbox.util.Matrix;
 
 /**
  * This is a basic example of Fugerit Venus Doc usage,
@@ -84,5 +91,91 @@ class DocHelperTest {
             log.info( "size {}:{}", outputFile.getName(), outputFile.length() );
         }
     }
+
+    public static final String ATT_WATERMARK_MODE = "watermarkMode";
+
+    public static final String ATT_WATERMARK_MODE_CUSTOM = "custom";
+
+    private File testWorker( String watermarkMode  ) throws Exception {
+        String chainId = "document";
+        // handler id
+        String handlerId = ID_PDF_FOP_PDF_UA;
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            // creates the doc helper
+            DocHelper docHelper = new DocHelper();
+            // create custom data for the fremarker template 'document.ftl'
+            List<People> listPeople = Arrays.asList(
+                    new People("Luthien", "Tinuviel", "Queen"), new People("Thorin", "Oakshield", "King"));
+
+            DocProcessContext context = DocProcessContext.newContext("listPeople", listPeople)
+                    .withAtt(ATT_WATERMARK_MODE, watermarkMode );
+
+            // output generation
+            docHelper.getDocProcessConfig().fullProcess(chainId, context, handlerId, baos);
+
+            File outputFile = new File( "target/", String.format( "%s-watermark-%s.%s", chainId, watermarkMode, DocConfig.TYPE_PDF ) );
+            log.info( "delete file? : {} ({})", outputFile.delete(), outputFile );
+
+            if ( ATT_WATERMARK_MODE_CUSTOM.equalsIgnoreCase( watermarkMode ) ) {
+                FileIO.writeBytes( addTextWatermark( baos.toByteArray(), "watermark" ), outputFile );
+            } else {
+                FileIO.writeBytes(baos.toByteArray(), outputFile);
+            }
+            return outputFile;
+        }
+    }
+
+    @Test
+    void testDocProcessTemplateWatermark() throws Exception {
+        File output = this.testWorker( "template" );
+        Assertions.assertNotEquals( 0, output.length() );
+    }
+
+    @Test
+    void testDocProcessCustomHandlerWatermark() throws Exception {
+        File output = this.testWorker( ATT_WATERMARK_MODE_CUSTOM );
+        Assertions.assertNotEquals( 0, output.length() );
+    }
+
+    private static byte[] addTextWatermark( byte[] input, String watermarkText) throws IOException {
+        try (PDDocument document = Loader.loadPDF( input );
+             ByteArrayOutputStream buffer = new ByteArrayOutputStream()) {
+
+            PDType1Font font = new PDType1Font(Standard14Fonts.FontName.HELVETICA_BOLD);
+
+            // Iterate through all pages
+            for (PDPage page : document.getPages()) {
+                PDPageContentStream contentStream = new PDPageContentStream(
+                        document, page, PDPageContentStream.AppendMode.APPEND, true, true);
+
+                // Set transparency
+                PDExtendedGraphicsState graphicsState = new PDExtendedGraphicsState();
+                graphicsState.setNonStrokingAlphaConstant(0.3f); // 30% opacity
+                contentStream.setGraphicsStateParameters(graphicsState);
+
+                // Set font and color
+                contentStream.setFont(font, 120);
+                contentStream.setNonStrokingColor( 0.2F, 0.2F, 0.2F); // Light gray
+
+                // Calculate position (center of page, diagonal)
+                float pageWidth = page.getMediaBox().getWidth();
+                float pageHeight = page.getMediaBox().getHeight();
+
+                // Position and rotate the text
+                contentStream.beginText();
+                contentStream.setTextMatrix(
+                        Matrix.getRotateInstance(Math.toRadians(45),
+                                pageWidth / 5,
+                                pageHeight / 5));
+                contentStream.showText(watermarkText);
+                contentStream.endText();
+
+                contentStream.close();
+            }
+            document.save(buffer);
+            return buffer.toByteArray();
+        }
+    }
+
 
 }
